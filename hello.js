5212932fdd371a93ae0124b72b5a031f40598ae7
dulguun <33824231+dulgunnnn@users.noreply.github.com>
2021-07-08 18:19:22 -0500

    uncomment task 1

--- hello.js
+++ hello.js
@@ -3,20 +3,20 @@ const readline = require("readline").createInterface({
   output: process.stdout,
 });
 
-// readline.question("What is your name? ", (name) => {
-//   console.log(`Welcome ${name}`);
+readline.question("What is your name? ", (name) => {
+  console.log(`Welcome ${name}`);
 
-//   readline.question("What is your age? ", (age) => {
-//     console.log(
-//       `You're ${
-//         age < 16
-//           ? "not allowed to drive in Iowa"
-//           : "allowed to get a drivers license in Iowa"
-//       }`
-//     );
-//     readline.close();
-//   });
-// });
+  readline.question("What is your age? ", (age) => {
+    console.log(
+      `You're ${
+        age < 16
+          ? "not allowed to drive in Iowa"
+          : "allowed to get a drivers license in Iowa"
+      }`
+    );
+    readline.close();
+  });
+});
 
 const counterModule = (function () {
   let counter = 0;
